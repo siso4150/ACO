e903705f519aeca786bfa3461d20ed1110f34e59
siso4150 <marimoss200341@gmail.com>
2026-01-13 16:06:52 +0900

    jsonでハイパーパラメータ指定機能を実装

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
-#include "helloworld.h"
+#include <iostream>
+#include <fstream>
 #include "json.hpp"
 
+using namespace std;
+using json = nlohmann::json;
+
+
+struct Config{
+  
+  double alpha;
+  double beta;
+  double rho;
+
+};
+
 int main() {
-    printHelloWorld();
+    //ハイパーパラメータのロード
+    Config config;
+
+    //ファイルの読み込み
+    ifstream f("hyperparameter.json");
+    //jsonのパース
+    json data = json::parse(f);
+
+    //各データを代入
+    config.alpha = data["alpha"];
+    config.beta  = data["beta"];
+    config.rho   = data["rho"];
+    
     return 0;
 }
